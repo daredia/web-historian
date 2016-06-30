dc34c2c917e4ddcdbfb2a7d2d4b2e29a56965b74
daredia <shehzad.daredia@gmail.com>
2016-06-29 21:03:10 -0700

    Implement POST request handler

--- web/request-handler.js
+++ web/request-handler.js
@@ -34,25 +34,64 @@ exports.handleRequest = function (req, res) {
         // debugger;
         res.end();
       });
+    } else {
+      //parse out URL path. This should be a name of a site.
+      myPath = archive.paths.archivedSites + req.url;
+      
+      //grab contents of file and write back to response body
+      var url = req.url.slice(1);
+
+      archive.isUrlArchived(url, function(exists) {
+        if (exists) {
+          //do something
+          fs.readFile(myPath, function (err, data) {
+            if (err) {
+              throw err; 
+            }
+            res.writeHead(200, helpers.headers);
+            //in response object, pass contents of file above.
+            res.write(data);
+            res.end();  
+          });      
+        } else {
+          res.writeHead(404, helpers.headers);
+          //in response object, pass contents of file above.
+          res.end();
+        }
+      });
     }
+  } else if ( req.method === 'POST') {
+    // request body will be a string containing a url 
+    var str = '';
+    //another chunk of data has been recieved, so append it to `str`
+    req.on('data', function (chunk) {
+      str += chunk;
+    });
+
+
+    //the whole response has been recieved, so we just print it out here
+    req.on('end', function () {
+      // once streaming of data is done, we'll have a stringified object
+      // grab url property of that object
+      var url = str.slice(4);
+      // pass that url into below helper
+      // check to see if url is NOT in the list (helper method)
+      archive.isUrlInList(url, function(exists) {
+        if (!exists) {
+          archive.addUrlToList(url, function() {
+            // respond to user with 302
+            res.writeHead(302, helpers.headers);
+            res.end();
+          });
+        }
+      });
+    });
+
     
-  }    
-   
-    //parse out URL path. This should be a name of a site.
-   
-    // var site = req.url.slice(1);
+      // if true, append url and \n to list
 
-    //if file is in list of URLs
-
-    //check to see if file already exists
-      //if no:
-        //create a file inside archives/sites where filename = name of site (www.google.com)
-        //inside file, write to file. write domain name. Ultimately html.
-      //if yes:
-        //grab contents of file
-    //in response object, pass contents of file above.
-
-
-
-  // res.end(archive.paths.list);
+      // TODO: if already archived, redirect user to /sitename 
+        // which will write response with site's html
+        // else, respond with loading.html
+  }
 };
