4153519aded575aaa4a21c3aa002681fa302dced
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-07-15 22:43:31 -0700

    Define Page mongoose model and api routes for creating and fetching from db

--- web/express-server.js
+++ web/express-server.js
@@ -1,20 +1,73 @@
 var express = require ('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Page = require('./models');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.port || 8080;
 
+mongoose.connect('mongodb://localhost/web-historian');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongo is connected!');
+});
+
 var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+  console.log('request was sent to api');
+  next();
+});
 
 // access at http://localhost:8080/api/
 router.get('/', function(req, res) {
   res.json({ message: 'welcome to our history items api!'});
 });
 
+router.route('/pages')
+  .post(function(req, res) {
+    var page = new Page();
+    
+    page.url = req.body.url || null;
+    page.title = req.body.title || null;
+    page.html = req.body.html || null;
+    page.text = req.body.text || null;
+    page.favicon = req.body.favicon || null;
+    page.hostname = req.body.hostname || null;
+    page.screenshot = req.body.screenshot || null;
+    page.referrer = req.body.referrer || null;
+    page.lastVisit = req.body.lastVisit || null;
+    page.visits = req.body.visits || null;
+
+    page.save(function(err) {
+      if (err) {
+        res.send(err);
+      }
+      res.json({ message: 'Page created!'} );
+    });
+
+  })
+
+  .get(function(req, res) {
+    Page.find(function(err, pages) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(pages);
+    });
+  });
+
+// register our /api routes
 app.use('/api', router);
 
 app.listen(port); 
 console.log('express server listening on port', port);
+
+
+
---
+++ web/models.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PageSchema = new Schema({
+  url: String,
+  title: String,
+  html: String,
+  text: String,
+  favicon: String,
+  hostname: String,
+  screenshot: String,
+  referrer: String,
+  lastVisit: Date,
+  visits: Number
+}, 
+  {
+    timestamps: true
+  });
+
+module.exports = mongoose.model('Page', PageSchema);
